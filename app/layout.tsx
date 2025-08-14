e5d7045c4fae4a00af0cf28c09f4c4df3db3d96d
SDSamarasinghe <it20605902@my.sliit.lk>
2025-08-14 22:41:43 +0530

    Refactor layout structure to include head section for favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,20 +15,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="h-screen bg-gray-50 font-sans antialiased overflow-hidden">
-        <NavigationProvider>
-          <div className="h-screen flex flex-col">
-            <CentralizedHeader />
-            <main className="flex-1 overflow-auto bg-gray-50">
-              <div className="min-h-full w-full">
-                {children}
-              </div>
-            </main>
-          </div>
-          <Toaster />
-        </NavigationProvider>
-      </body>
-    </html>
+      <html lang="en">
+        <head>
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        </head>
+        <body className="h-screen bg-gray-50 font-sans antialiased overflow-hidden">
+          <NavigationProvider>
+            <div className="h-screen flex flex-col">
+              <CentralizedHeader />
+              <main className="flex-1 overflow-auto bg-gray-50">
+                <div className="min-h-full w-full">
+                  {children}
+                </div>
+              </main>
+            </div>
+            <Toaster />
+          </NavigationProvider>
+        </body>
+      </html>
   );
 }
